0fa4c55908b901d19a26c5aea2026a6695cf9c0a
Luis Cruz <ljc2491@gmail.com>
2014-03-14 11:07:58 -0500

    mm: compaction: Fix conflicting compact_node types
    Since commit c0b9652, compact_nodes was channged from an int to a
    void. Update the earlysuspend usage to do the same and fix the
    build.
    
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -789,7 +789,7 @@ unsigned long try_to_compact_pages(struct zonelist *zonelist,
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
 static struct work_struct compactnodes_w;
-static int compact_nodes(void);
+static void compact_nodes(void);
 static void compactnodes_work(struct work_struct *w)
 {
 	/* No point in being gun shy here since compact_zone()

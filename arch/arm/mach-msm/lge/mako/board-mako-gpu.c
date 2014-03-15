f2225b06b9f2a33ba45582483d507be8aec804a7
Jake Weinstein <xboxlover360@gmail.com>
2014-03-15 12:39:48 -0500

    Revert "Better GPU overclock, including 400MHz, modified bw, up to 533mhz"
    This reverts commit b2d06f592ccfd132f58d2f4a711c7cbd08235946.

--- arch/arm/mach-msm/lge/mako/board-mako-gpu.c
+++ arch/arm/mach-msm/lge/mako/board-mako-gpu.c
@@ -227,16 +227,19 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
+#ifdef CONFIG_GPU_OVERCLOCK
 		{
 			.gpu_freq = 487500000,
-			.bus_freq = 5,
+			.bus_freq = 4,
 			.io_fraction = 0,
 		},
+#else
         {
 			.gpu_freq = 400000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
+#endif
 		{
 			.gpu_freq = 320000000,
 			.bus_freq = 3,
@@ -258,7 +261,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
-	.num_levels = 6,
+	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.strtstp_sleepwake = true,
